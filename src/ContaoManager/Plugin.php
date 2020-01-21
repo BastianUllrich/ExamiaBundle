aefb97e0abf8135f531660a106ec159ccac8d8be
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-21 10:51:55 +0100

    Bundle repariert

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -14,7 +14,7 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Baul\ExamiaBundle\BaulTestBundle;
+use Baul\ExamiaBundle\BaulExamiaBundle;
 
 class Plugin implements BundlePluginInterface
 {

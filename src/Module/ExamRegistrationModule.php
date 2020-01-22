cd5e5dd9e2dc3893e394d07358f20ba8ad203815
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-22 19:47:43 +0100

    Test ExamRegisterModule Absenden

--- src/Module/ExamRegistrationModule.php
+++ src/Module/ExamRegistrationModule.php
@@ -110,7 +110,7 @@ class ExamRegistrationModule extends \Module
         $this->import('Database');
 
         $set = array('tstamp' => time(), 'title' => $exam_title, 'lecturer_title' => $lecturer_title, 'lecturer_prename' => $lecturer_firstname, 'lecturer_lastname' => $lecturer_lastname,
-                    'lecturer_email' => $lecturer_email, 'lecturer_mobile' => $lecturer_mobile, 'department' => $department, 'date' => $exam_date, 'exam_begin' => $exam_begin,
+                    'lecturer_email' => $lecturer_email, 'lecturer_mobile' => $lecturer_mobile, 'department' => $department, 'date' => $exam_date, 'begin' => $exam_begin,
                     'duration' => $exam_duration, 'tools' => $tools, 'remarks' => $remarks, 'status' => $status);
 
         if ($objInsert = $this->Database->prepare("INSERT INTO tl_exams %s")->set($set)->execute()) {

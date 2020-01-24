13dee1cf42c876224c77e889d0bb05c23cba5518
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-24 16:21:43 +0100

    Modul Mitglieder Verwaltung bearbeitet (Funktion erstellt)

--- src/Module/MemberAdministrationModule.php
+++ src/Module/MemberAdministrationModule.php
@@ -45,7 +45,7 @@ class MemberAdministrationModule extends \Module
 
         // Daten des Mitglieds aus der Datenbank laden
         $this->import('Database');
-        $allMembers = Database::getInstance()->prepare("SELECT * FROM tl_member ORDER BY disable")->query();
+        $allMembers = Database::getInstance()->prepare("SELECT * FROM tl_member ORDER BY disable DESC")->query();
         $i = 0;
         $memberData = array();
 

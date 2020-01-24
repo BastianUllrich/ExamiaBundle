36253c9b638cdb874405e9ae2871532a54a9c382
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-24 09:48:28 +0100

    Unsuscribe Modul angepasst

--- src/Module/ExamUnsuscribeModule.php
+++ src/Module/ExamUnsuscribeModule.php
@@ -85,6 +85,7 @@ class ExamUnsuscribeModule extends \Module
         // Von Klausur abmelden
         if (($_GET["do"] == "unsuscribe")) {
             $exam_id = $_GET["exam"];
+            $this->import('Database');
             if ($unsuscribeFromExam = $this->Database->prepare("DELETE FROM tl_attendees_exams WHERE exam_id=%s AND attendee_id=%s")->set($exam_id, $userID)->execute()) {
                 $this->Template->unsuscribtionSuccess = $GLOBALS['TL_LANG']['miscellaneous']['unsuscribtionSuccess'];
                 \Controller::redirect('klausurverwaltung/von-klausur-abmelden.html');

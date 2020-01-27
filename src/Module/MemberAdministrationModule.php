17c54decb773bdd87fe1d900886b625d8bb320f4
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-27 15:47:16 +0100

    MemberAdministration Update

--- src/Module/MemberAdministrationModule.php
+++ src/Module/MemberAdministrationModule.php
@@ -340,6 +340,7 @@ class MemberAdministrationModule extends \Module
         }
 
         if($this->Database->prepare("UPDATE tl_member %s WHERE id=$member")->set($set)->execute()) {
+            \Controller::redirect('benutzerbereich/mitglieder-verwalten.html?do=editDetails&member=$member');
             $this->Template->changesSaved = true;
             $this->Template->changesSavedMessage = $GLOBALS['TL_LANG']['miscellaneous']['changesSavedMessage'];
         }

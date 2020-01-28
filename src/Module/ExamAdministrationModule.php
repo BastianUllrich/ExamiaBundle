1a1ee1637a9d56b9e44b6af2700503b1f90bccec
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-28 14:13:59 +0100

    Klausurverwaltung - Details Test späteste Endzeit

--- src/Module/ExamAdministrationModule.php
+++ src/Module/ExamAdministrationModule.php
@@ -199,8 +199,9 @@ class ExamAdministrationModule extends \Module
             }
         }
         // Späteste Endzeit = Startzeit + Maximale Dauer über date()- und time()-Funktion
-        $this->Template->detailMaxEndtime = date("H:i", (time(($examDetails->date+($maxDuration*60)))));
-        //$this->Template->detailMaxEndtime = $examDetails->begin + $maxDuration;
+        //$this->Template->detailMaxEndtime = date("H:i", (time(($examDetails->date+($maxDuration*60)))));
+        $this->Template->detailMaxEndtime = $maxDuration;
+        $this->Template->detailMaxEndtime .= "Minuten";
 
         // Dozent zusammensetzen
         $this->Template->detailLecturer = $examDetails->lecturer_title;

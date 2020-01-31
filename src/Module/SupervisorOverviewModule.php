f6eed818a74c49cb2d9021f9b6187ee06a4b5d54
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-01-31 12:21:19 +0100

    Aufsichtsübersicht

--- src/Module/SupervisorOverviewModule.php
+++ src/Module/SupervisorOverviewModule.php
@@ -73,10 +73,10 @@ class SupervisorOverviewModule extends \Module
 
 
         for ($i = 0; $i < sizeof($supervisorData); $i++) {
-            //$min_time_question = Database::getInstance()->prepare("SELECT MIN(time_from) AS 'mintime' FROM tl_supervisors_exams WHERE supervisor_id = $userID AND date = $supervisorData[$i]['time']");
-            //$max_time_question = Database::getInstance()->prepare("SELECT MAX(time_until) AS 'maxtime' FROM tl_supervisors_exams WHERE supervisor_id = $userID AND date = $supervisorData[$i]['time']");
-            $supervisorData[$i]['begin'] = $supervisorData[$i]['time'];
-            $supervisorData[$i]['end'] = $supervisorData[$i]['time'];
+            $min_time_question = Database::getInstance()->prepare("SELECT MIN(time_from) AS 'mintime' FROM tl_supervisors_exams WHERE supervisor_id = $userID AND date = $supervisorData[$i]['time']");
+            $max_time_question = Database::getInstance()->prepare("SELECT MAX(time_until) AS 'maxtime' FROM tl_supervisors_exams WHERE supervisor_id = $userID AND date = $supervisorData[$i]['time']");
+            $supervisorData[$i]['begin'] = $min_time_question['mintime'];
+            $supervisorData[$i]['end'] = $max_time_question['maxtime'];
         }
         $this->Template->supervisorDataList = $supervisorData;
 

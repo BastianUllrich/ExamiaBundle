216aebb2c2ef5a7e6fc9476e69dde4ff4c338103
Bastian Ullrich <bastian.ullrich@iem.thm.de>
2020-02-09 13:17:39 +0100

    Anpassung Aufsichtsverwaltung

--- src/Module/SupervisorAdministrationModule.php
+++ src/Module/SupervisorAdministrationModule.php
@@ -140,12 +140,12 @@ class SupervisorAdministrationModule extends \Module
             $supervisorData[$i]['timePeriod'] .= $result->time_until;
             $supervisorData[$i]['task'] = $result->task;
             if ($result->task == "Aufsicht") {
-                $this->Template->$supervisorData[$i]['linkTitleDelete'] = $GLOBALS['TL_LANG']['miscellaneous']['linkTitleDeleteSupervisor'];
-                $this->Template->$supervisorData[$i]['deleteText'] = $GLOBALS['TL_LANG']['miscellaneous']['deleteSupervisorText'];
+                $supervisorData[$i]['linkTitleDelete'] = $GLOBALS['TL_LANG']['miscellaneous']['linkTitleDeleteSupervisor'];
+                $supervisorData[$i]['deleteText'] = $GLOBALS['TL_LANG']['miscellaneous']['deleteSupervisorText'];
             }
             if ($result->task == "Schreibassistenz") {
-                $this->Template->$supervisorData[$i]['linkTitleDelete'] = $GLOBALS['TL_LANG']['miscellaneous']['linkTitleDeleteAssistance'];
-                $this->Template->$supervisorData[$i]['deleteText'] = $GLOBALS['TL_LANG']['miscellaneous']['deleteAssistanceText'];
+                $supervisorData[$i]['linkTitleDelete'] = $GLOBALS['TL_LANG']['miscellaneous']['linkTitleDeleteAssistance'];
+                $supervisorData[$i]['deleteText'] = $GLOBALS['TL_LANG']['miscellaneous']['deleteAssistanceText'];
             }
             $i++;
         }
